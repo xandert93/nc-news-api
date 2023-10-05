212407064c96005682e35a71379f2a5be51e352c
unknown <xandert.93@outlook.com>
2023-10-05 11:19:54 +0100

    prepare for deployment again

--- db/connection.js
+++ db/connection.js
@@ -3,6 +3,7 @@ const path = require('path')
 
 // when Jest runs, it sets NODE_ENV=test. Else assume 'development'
 const mode = process.env.NODE_ENV || 'development'
+const isInProduction = mode === 'production'
 
 require('dotenv').config({
   path: path.join(__dirname, `/../.env.${mode}`),
@@ -11,6 +12,15 @@ require('dotenv').config({
 // prevent connection to default DB of `postgres`:
 if (!process.env.PGDATABASE) throw new Error('PGDATABASE not set')
 
-const db = new pg.Pool()
+if (isInProduction && !process.env.PGDATABASE_URL) {
+  throw new Error('Production PGDATABASE_URL not set')
+}
+
+const db = new pg.Pool({
+  ...(isInProduction && {
+    connectionString: process.env.PGDATABASE_URL,
+    max: 2,
+  }),
+})
 
 module.exports = db
